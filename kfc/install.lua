8752636a935c27dfde363fb206e83c4cc845f787
YRNBAM <145809841+bigbam974@users.noreply.github.com>
2024-10-01 17:49:08 -0400

    Add files via upload

---
+++ kfc/install.lua
@@ -0,0 +1,56 @@
+this is to just getr them into the server you have to make a shop accordingly to the script your using
+
+join discords for support
+
+-------------------------------QB/QB-Core/Shared/Items-----------------------------------
+ 
+
+-- Food
+	kfc-snacker 				 = {["name"] = "kfc-snacker", 			 		["label"] = "Snacker", 					["weight"] = 250, 		["type"] = "item", 		["image"] = "kfc-snacker.png", 					["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "Sates Hunger."},
+	kfc-zingerburger 			 = {["name"] = "kfc-zingerburger", 			 	["label"] = "ZingerBurger", 			["weight"] = 300, 		["type"] = "item", 		["image"] = "kfc-zingerburger.png", 			["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "Sates Hunger."},
+	kfc-submarine 				 = {["name"] = "kfc-submarine", 			 	["label"] = "Chicken Submarine", 		["weight"] = 310, 		["type"] = "item", 		["image"] = "kfc-submarine.png", 				["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "Sates Hunger."},
+	kfc-heartstopper 		 	 = {["name"] = "kfc-heartstopper", 				["label"] = "Heartstopper", 			["weight"] = 2500, 		["type"] = "item", 		["image"] = "kfc-heartstopper.png", 			["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "Sates Hunger."},
+	kfc-veggieburger 		 	 = {["name"] = "kfc-veggieburger", 				["label"] = "Veggie Burger", 			["weight"] = 125, 		["type"] = "item", 		["image"] = "kfc-veggieburger.png", 			["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "Sates Hunger."},
+	kfc-fries 				 	 = {["name"] = "kfc-fries", 			 	  	["label"] = "Fries", 					["weight"] = 125, 		["type"] = "item", 		["image"] = "kfc-fries.png", 					["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "Sates Hunger."},
+		-- Drinks
+
+	kfc-softdrink 			 	 = {["name"] = "kfc-softdrink", 				["label"] = "Soft Drink", 				["weight"] = 125, 		["type"] = "item", 		["image"] = "kfc-softdrink.png", 				["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "An Ice Cold Drink."},
+	kfc-mshake 			     	 = {["name"] = "kfc-mshake", 					["label"] = "Milkshake", 				["weight"] = 125, 		["type"] = "item", 		["image"] = "kfc-mshake.png", 					["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "Hand-scooped for you!"},
+
+
+--------------------------QB/QB-Smallresources/Config-------------------------------
+
+		-- kfc
+			["kfc-snacker"] = math.random(35, 54),
+			["kfc-zingerburger"] = math.random(35, 54),
+			["kfc-submarine"] = math.random(35, 54),
+			["kfc-veggieburger"] = math.random(35, 54),
+			["kfc-heartstopper"] = math.random(35, 54),
+			["kfc-fries"] = math.random(35, 54),
+		
+		
+		
+			 -- kfc
+			["kfc-softdrink"] = math.random(40, 50),
+			["kfc-mshake"] = math.random(40, 50),
+
+
+
+
+-----------------------ADD IMAGES TO QB/QB-inventory/HTML/Images--------------------------
+
+
+Enjoyyyyyyyyy
+
+___YRNBAM
+
+join my discords
+
+--Cars&Things
+https://discord.gg/h3deXeGcBC
+
+--SDRP.V1
+https://discord.gg/f5BR4AVBev
+
+--Westpalm100kordie
+https://discord.gg/f5BR4AVBev
